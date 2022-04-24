b2d2aeee8864e1c6837c1614a6ae1d8d5b4536b7
Denis Moshkin <denis.moshkin@fleetcor.ru>
2022-04-24 20:28:32 +0300

    add BurgerIngredient component

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import {CurrencyIcon, Counter, Tab} from '@ya.praktikum/react-developer-burger-ui-components';
+import {Tab} from '@ya.praktikum/react-developer-burger-ui-components';
+import BurgerIngredient from "../burger-ingredient/burger-ingredient";
 import style from './burger-ingredients.module.css';
 import PropTypes from 'prop-types';
 import ingredientPropTypes from "../../constants/ingredient-prop-types";
@@ -31,17 +32,9 @@ function BurgerIngredients(props){
                             <section key={index}>
                                 <h3 className={` text_type_main-medium`}>{categories[key]}</h3>
                                 <div className={`${style.section} pt-6 pl-4 pr-4 pb-2`}>
-                                    {ingredients.filter(({type}) => type === key).map(({_id, image, name, price}, index) => {
+                                    {ingredients.filter(({type}) => type === key).map((ingredient, index) => {
                                         return (
-                                            <div className={`${style.item} mb-8`} key={_id}>
-                                                {index % 5 === 0 && <Counter count={1}/>}
-                                                <img className={`ml-4 mr-4`} src={image} alt={name}/>
-                                                <div className={`${style.price} pt-1 pb-1`}>
-                                                    <span className={"mr-2 text text_type_digits-default"}>{price}</span>
-                                                    <CurrencyIcon type={"primary"}/>
-                                                </div>
-                                                <span className={`${style.name} text_type_main-default`}>{name}</span>
-                                            </div>
+                                            <BurgerIngredient key={ingredient._id} counter={index % 5 === 0 ? 1 : 0} {...ingredient}/>
                                         )
                                     })}
                                 </div>
