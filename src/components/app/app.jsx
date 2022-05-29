dfe519cd6dffd0d29942e8eb7414b44a736f0828
Denis Moshkin <denis.moshkin@fleetcor.ru>
2022-05-29 18:37:41 +0300

    create basic pages structure

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -9,7 +9,15 @@ import {Provider} from "react-redux";
 import {store} from "../../services/store";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import { NotFound404, HomePage } from "../../pages";
+import {
+    NotFound404,
+    HomePage,
+    LoginPage,
+    RegisterPage,
+    ForgotPasswordPage,
+    ResetPasswordPage,
+    ProfilePage, IngredientPage
+} from "../../pages";
 
 function App(){
 
@@ -21,6 +29,24 @@ function App(){
                     <Switch>
                         <Route path="/" exact={true}>
                             <HomePage/>
+                        </Route>
+                        <Route path="/login" exact={true}>
+                            <LoginPage/>
+                        </Route>
+                        <Route path="/register" exact={true}>
+                            <RegisterPage/>
+                        </Route>
+                        <Route path="/forgot-password" exact={true}>
+                            <ForgotPasswordPage/>
+                        </Route>
+                        <Route path="/reset-password" exact={true}>
+                            <ResetPasswordPage/>
+                        </Route>
+                        <Route path="/profile" exact={true}>
+                            <ProfilePage/>
+                        </Route>
+                        <Route path="/ingredients/:id" exact={true}>
+                            <IngredientPage/>
                         </Route>
                         <Route>
                             <NotFound404 />
