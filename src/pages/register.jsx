522f4945385d5cb568011818018a78db43172b7e
Denis Moshkin <denis.moshkin@fleetcor.ru>
2022-05-29 21:07:34 +0300

    add register page

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
+import styles from './login.module.css'
+import {Button, EmailInput, Input, PasswordInput} from "@ya.praktikum/react-developer-burger-ui-components";
+import {Link, useHistory} from "react-router-dom";
 
 export function RegisterPage() {
-    return (
-        <div>
 
+    const [form, setValue] = useState({ name: '', email: '', password: '' });
+
+    const onChange = e => {
+        setValue({ ...form, [e.target.name]: e.target.value });
+    };
+
+    const history = useHistory();
+    return (
+        <div className={`${styles.wrapper} pt-30 mt-15`}>
+            <div className={`${styles.container}`}>
+                <p className={`text text_type_main-medium mb-6`}>Регистрация</p>
+                <div className="mb-6">
+                    <Input
+                        type={'text'}
+                        placeholder={'Иия'}
+                        onChange={onChange}
+                        value={form.name}
+                        name={'name'}
+                        error={false}
+                        errorText={'Ошибка'}
+                        size={'default'}
+                    />
+                </div>
+                <div className="mb-6">
+                    <EmailInput onChange={onChange} size={undefined} value={form.email} name={'email'} />
+                </div>
+                <div className="mb-6">
+                    <PasswordInput onChange={onChange} value={form.password} name={'password'} />
+                </div>
+                <Button type="primary" size="large">
+                    Зарегистрироваться
+                </Button>
+                <p className={`text mt-20 text_type_main-default text_color_inactive`}>Уже зарегистрировались?  <Link to={{ pathname: `/login`, state: history.location.state }} className={`text_color_accent`}>Войти</Link></p>
+            </div>
         </div>
     );
 }
