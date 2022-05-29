7a56a62f7050c7ba4cb6d58f447d53bd82c67361
Denis Moshkin <denis.moshkin@fleetcor.ru>
2022-05-29 23:16:10 +0300

    add profile page

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import AppHeader from "../app-header/app-header";
-import BurgerIngredients from "../burger-ingredients/burger-ingredients";
-import BurgerConstructor from "../burger-constructor/burger-constructor";
-import appStyles from './app.module.css';
+
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import {Provider} from "react-redux";
 import {store} from "../../services/store";
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {Route, Switch, BrowserRouter as Router} from 'react-router-dom';
 
 import {
     NotFound404,
@@ -22,40 +20,40 @@ import {
 function App(){
 
     return (
+        <Router>
         <Provider store={store}>
             <DndProvider backend={HTML5Backend}>
                 <AppHeader/>
-                <Router>
-                    <Switch>
-                        <Route path="/" exact={true}>
-                            <HomePage/>
-                        </Route>
-                        <Route path="/login" exact={true}>
-                            <LoginPage/>
-                        </Route>
-                        <Route path="/register" exact={true}>
-                            <RegisterPage/>
-                        </Route>
-                        <Route path="/forgot-password" exact={true}>
-                            <ForgotPasswordPage/>
-                        </Route>
-                        <Route path="/reset-password" exact={true}>
-                            <ResetPasswordPage/>
-                        </Route>
-                        <Route path="/profile" exact={true}>
-                            <ProfilePage/>
-                        </Route>
-                        <Route path="/ingredients/:id" exact={true}>
-                            <IngredientPage/>
-                        </Route>
-                        <Route>
-                            <NotFound404 />
-                        </Route>
-                    </Switch>
-                </Router>
-
+                <Switch>
+                    <Route path="/" exact={true}>
+                        <HomePage/>
+                    </Route>
+                    <Route path="/login" exact={true}>
+                        <LoginPage/>
+                    </Route>
+                    <Route path="/register" exact={true}>
+                        <RegisterPage/>
+                    </Route>
+                    <Route path="/forgot-password" exact={true}>
+                        <ForgotPasswordPage/>
+                    </Route>
+                    <Route path="/reset-password" exact={true}>
+                        <ResetPasswordPage/>
+                    </Route>
+                    <Route path="/profile" exact={true}>
+                        <ProfilePage/>
+                    </Route>
+                    <Route path="/ingredients/:id" exact={true}>
+                        <IngredientPage/>
+                    </Route>
+                    <Route>
+                        <NotFound404 />
+                    </Route>
+                </Switch>
             </DndProvider>
         </Provider>
+        </Router>
+
     );
 }
 
--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,9 +1,76 @@
-import React from 'react';
+import React, {useState} from 'react';
+import styles from './profile.module.css'
+import {Button, EmailInput, Input, PasswordInput} from "@ya.praktikum/react-developer-burger-ui-components";
+import {Link, NavLink, useHistory} from "react-router-dom";
 
 export function ProfilePage() {
-    return (
-        <div>
+    const [form, setValue] = useState({ name: '', email: '', password: '' });
 
+    const onChange = e => {
+        setValue({ ...form, [e.target.name]: e.target.value });
+    };
+
+    const history = useHistory();
+    return (
+        <div className={`${styles.wrapper} pt-30 mt-15`}>
+            <div className={`${styles.container}`}>
+                <section className={`${styles.nav} mr-15`}>
+                    <nav>
+                        <ul>
+                            <li className={`pt-4 pb-4 text text_type_main-medium  ${styles.borderDashedAccent}`}>
+                                <NavLink
+                                    exact
+                                    to={{ pathname: `/profile` }}
+                                    className={`text_color_inactive`}
+                                    activeClassName={styles.activeLink}
+                                >Профиль</NavLink>
+                            </li>
+                            <li className={`pt-4 pb-4 text text_type_main-medium ${styles.borderTopNone} ${styles.borderDashedAccent}`}>
+                                <NavLink
+                                    exact
+                                    to={{ pathname: `/profile/orders` }}
+                                    className={`text_color_inactive`}
+                                    activeClassName={styles.activeLink}
+                                >История заказов</NavLink>
+                            </li>
+                            <li className={`pt-4 pb-4 text text_type_main-medium ${styles.borderTopNone} ${styles.borderDashedAccent}`}>
+                                <NavLink
+                                    exact
+                                    to={{ pathname: `/logout` }}
+                                    className={`text_color_inactive`}
+                                    activeClassName={styles.activeLink}
+                                >Выход</NavLink>
+                            </li>
+                        </ul>
+                    </nav>
+                    <div className={`mt-20 ${styles.borderDashedAccent}`}>
+                        <p className={`text_color_inactive text_type_main-default ${styles.navInfo}`}>В этом разделе вы можете
+                            изменить свои персональные данные</p>
+                    </div>
+
+                </section>
+                <section>
+                    <div className="mb-6">
+                        <Input
+                            type={'text'}
+                            placeholder={'Иия'}
+                            onChange={onChange}
+                            value={form.name}
+                            name={'name'}
+                            error={false}
+                            errorText={'Ошибка'}
+                            size={'default'}
+                        />
+                    </div>
+                    <div className="mb-6">
+                        <EmailInput onChange={onChange} size={undefined} value={form.email} name={'email'} />
+                    </div>
+                    <div className="mb-6">
+                        <PasswordInput onChange={onChange} value={form.password} name={'password'} />
+                    </div>
+                </section>
+
+            </div>
         </div>
     );
 }
