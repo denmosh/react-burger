c7c03c4f9c3d0c18776aab2808554ae817008580
Denis Moshkin <denis.moshkin@fleetcor.ru>
2022-06-20 03:07:39 +0300

    add protected route

---
+++ src/components/protected-route/protected-route.jsx
@@ -0,0 +1,47 @@
+import { Redirect, Route } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import {useDispatch, useSelector} from "react-redux";
+import {getUser} from "../../services/actions/user";
+
+export function ProtectedRoute({ children, ...rest }) {
+
+    let { user,  getUserRequest, tokenRequest} = useSelector(store => store.user)
+    const [isUserLoaded, setUserLoaded] = useState(false);
+
+    const dispatch = useDispatch();
+
+    const init = () => {
+        if(user.email){
+            setUserLoaded(true);
+        }else{
+            dispatch(getUser());
+            setUserLoaded(true);
+        }
+
+    };
+
+    useEffect(() => {
+        init();
+    }, []);
+
+    if(!isUserLoaded || getUserRequest || tokenRequest){
+        return null;
+    }
+    return (
+        <Route
+            {...rest}
+            render={({ location }) =>
+                user.email ? (
+                    children
+                ) : (
+                    <Redirect
+                        to={{
+                            pathname: '/login',
+                            state: { from: location }
+                        }}
+                    />
+                )
+            }
+        />
+    );
+}
