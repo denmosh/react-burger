536e8dae1078357d3ebedf25d4e4022cbaceadfa
Denis.Moshkin <moshkin.den@gmail.com>
2022-08-06 14:51:23 +0300

    update order-dashboard.tsx

--- src/components/order-dashboard/order-dashboard.tsx
+++ src/components/order-dashboard/order-dashboard.tsx
@@ -1,32 +1,46 @@
 import React from 'react';
 import styles from  "./order-dashboard.module.css";
+import {useAppSelector} from "../../hooks/hooks";
 
 function OrderDashboard() {
+    const {total, totalToday, orders} = useAppSelector(store => store.wsReducer)
+    const done = orders.filter((order)=>{
+        return order.status === 'done';
+    })
+    const prepared = orders.filter((order)=>{
+        return order.status === 'prepared';
+    })
     return (
         <div className={`mr-15`}>
             <div className={`${styles.numberContainer} mb-15`}>
                 <div className={`${styles.numberCol}`}>
                     <h3 className={`text_type_main-medium mb-6`}>Готовы:</h3>
                     <ul>
-                        <li className={`text_color_success text_type_digits-default mb-2`}>034533</li>
-                        <li className={`text_color_success text_type_digits-default mb-2`}>034534</li>
+                        {done.slice(0, 6).map((order)=>{
+                            return(
+                                <li className={`text_color_success text_type_digits-default mb-2`}>{order.number}</li>
+                            )
+                        })}
                     </ul>
                 </div>
                 <div className={`${styles.numberCol}`}>
                     <h3 className={`text_type_main-medium mb-6`}>В работе:</h3>
                     <ul>
-                        <li className={`text_color_primary text_type_digits-default mb-2`}>034533</li>
-                        <li className={`text_color_primary text_type_digits-default mb-2`}>034534</li>
+                        {prepared.slice(0, 6).map((order)=>{
+                            return(
+                                <li className={`text_color_primary text_type_digits-default mb-2`}>{order.number}</li>
+                            )
+                        })}
                     </ul>
                 </div>
             </div>
             <div className={`mb-15`}>
                 <h3 className={`text_type_main-medium mb-6`}>Выполнено за все время:</h3>
-                <span className={`${styles.totalNumber} text_type_digits-large`}>28 752</span>
+                <span className={`${styles.totalNumber} text_type_digits-large`}>{total}</span>
             </div>
             <div className={`mb-15`}>
                 <h3 className={`text_type_main-medium mb-6`}>Выполнено за сегодня:</h3>
-                <span className={`${styles.totalNumber} text_type_digits-large`}>138</span>
+                <span className={`${styles.totalNumber} text_type_digits-large`}>{totalToday}</span>
             </div>
 
 
--- src/components/order-item/order-item.tsx
+++ src/components/order-item/order-item.tsx
@@ -3,6 +3,8 @@ import styles from './order-item.module.css';
 import {CurrencyIcon} from "@ya.praktikum/react-developer-burger-ui-components";
 import {IIngredient, IOrderItem} from "../../services/interfaces/interfaces";
 import {useAppSelector} from "../../hooks/hooks";
+import Moment from 'moment';
+import 'moment/locale/ru'
 
 type Porps = {
     order:IOrderItem;
@@ -12,9 +14,10 @@ function OrderItem({order}:Porps) {
     const {ingredients, name, createdAt, status, number} = order
     const ingredientsDetailed = useAppSelector(state => state.burgerIngredients.ingredients);
     const ingredientsRender = []
+    Moment.locale('ru');
 
 
-    for(let i = 0; i < ingredients.length && i!==3; i++){
+    for(let i = 0; i < ingredients.length && i!==6; i++){
         let ingredient = ingredientsDetailed.find(x => x._id === ingredients[i])
             ingredientsRender.push(
                 <div key={i} className={styles.iconWrapper} style={{
@@ -23,7 +26,7 @@ function OrderItem({order}:Porps) {
                     left: `-${i * 15}px`
                 }}>
                     <div className={styles.icon}></div>
-                    {i === 2 && ingredients.length > 3 && (<div
+                    {i === 5 && ingredients.length > 6 && (<div
                         className={`text_type_main-default ${styles.iconNumbers}`}>+{ingredients.length - 3}</div>)}
                 </div>
             )
@@ -44,7 +47,7 @@ function OrderItem({order}:Porps) {
         <div className={`${styles.container} p-6 mb-6`}>
             <div className={`${styles.topContainer} mb-6`}>
                 <span className={`text_type_digits-default `}>#{number}</span>
-                <span className={`text_type_main-default text_color_inactive ${styles.date}`}>Сегодня, 16:20 i-GMT+3</span>
+                <span className={`text_type_main-default text_color_inactive ${styles.date}`}>{Moment(createdAt).calendar()} i-GMT+3</span>
             </div>
             <h3 className={`text_type_main-medium mb-6`}>{name}</h3>
             <div className={`${styles.bottomContainer}`}>
